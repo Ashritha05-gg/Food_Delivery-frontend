7b12c02ac7160b7371e018de581fbb013fa08238
Ashritha05-gg <kashrithak@gmail.com>
2025-05-17 20:38:41 +0530

    First commit

---
+++ src/assets/assets.js
@@ -0,0 +1,126 @@
+// UI & icon assets
+import logo from './logo.png';
+import add_icon from './add_icon.png';
+import order_icon from './order_icon.png';
+import profile_image from './profile_image.png';
+import upload_area from './upload_area.png';
+import parcel_icon from './parcel_icon.png';
+import search_icon from './search_icon.png';
+import basket_icon from './basket_icon.png';
+
+// Menu category images
+import menu_1 from './menu_1.png';
+import menu_2 from './menu_2.png';
+import menu_3 from './menu_3.png';
+import menu_4 from './menu_4.png';
+import menu_5 from './menu_5.png';
+import menu_6 from './menu_6.png';
+import menu_7 from './menu_7.png';
+import menu_8 from './menu_8.png';
+
+// Food item images
+import food_1 from './food_1.png';
+import food_2 from './food_2.png';
+import food_3 from './food_3.png';
+import food_4 from './food_4.png';
+import food_5 from './food_5.png';
+import food_6 from './food_6.png';
+import food_7 from './food_7.png';
+import food_8 from './food_8.png';
+import food_9 from './food_9.png';
+import food_10 from './food_10.png';
+import food_11 from './food_11.png';
+import food_12 from './food_12.png';
+import food_13 from './food_13.png';
+import food_14 from './food_14.png';
+import food_15 from './food_15.png';
+import food_16 from './food_16.png';
+import food_17 from './food_17.png';
+import food_18 from './food_18.png';
+import food_19 from './food_19.png';
+import food_20 from './food_20.png';
+import food_21 from './food_21.png';
+import food_22 from './food_22.png';
+import food_23 from './food_23.png';
+import food_24 from './food_24.png';
+import food_25 from './food_25.png';
+import food_26 from './food_26.png';
+import food_27 from './food_27.png';
+import food_28 from './food_28.png';
+import food_29 from './food_29.png';
+//import food_30 from './food_30.png';
+
+import rating_starts from './rating_starts.png';
+import remove_icon_red from './remove_icon_red.png';
+import add_icon_green from './add_icon_green.png';
+import add_icon_white from './add_icon_white.png';
+import facebook_icon from './facebook_icon.png';
+import twitter_icon from './twitter_icon.png';
+import linkedin_icon from './linkedin_icon.png';
+import cross_icon from './cross_icon.png';
+
+export const assets = {
+  logo,
+  add_icon,
+  order_icon,
+  profile_image,
+  upload_area,
+  parcel_icon,
+  search_icon,
+  basket_icon,
+  rating_starts,
+  remove_icon_red,
+  add_icon_green,
+  add_icon_white,
+  facebook_icon,
+  twitter_icon,
+  linkedin_icon,
+  cross_icon
+};
+
+export const menu_list = [
+  { menu_name: "Salad", menu_image: menu_1 },
+  { menu_name: "Pizza", menu_image: menu_2 },
+  { menu_name: "Burger", menu_image: menu_3 },
+  { menu_name: "Pasta", menu_image: menu_4 },
+  { menu_name: "Noodles", menu_image: menu_5 },
+  { menu_name: "Fries", menu_image: menu_6 },
+  { menu_name: "Rice", menu_image: menu_7 },
+  { menu_name: "Dessert", menu_image: menu_8 },
+];
+
+export const food_list = [
+  { _id: "1", name: "Greek Salad", image: food_1, price: 12, description: "Fresh salad with olives and feta.", category: "Salad" },
+  { _id: "2", name: "Caesar Salad", image: food_2, price: 10, description: "Classic Caesar with croutons.", category: "Salad" },
+  { _id: "4", name: "Pepperoni Pizza", image: food_4, price: 18, description: "Loaded with pepperoni.", category: "Pizza" },
+  { _id: "5", name: "Veggie Pizza", image: food_5, price: 16, description: "Bell peppers, mushrooms, olives.", category: "Pizza" },
+  { _id: "6", name: "Beef Burger", image: food_6, price: 14, description: "Juicy grilled beef patty.", category: "Burger" },
+  { _id: "7", name: "Chicken Burger", image: food_7, price: 13, description: "Crispy chicken with lettuce.", category: "Burger" },
+  { _id: "8", name: "Cheese Burger", image: food_8, price: 12, description: "Melted cheese and pickles.", category: "Burger" },
+  { _id: "9", name: "Spaghetti Bolognese", image: food_9, price: 17, description: "Meaty Italian pasta.", category: "Pasta" },
+  { _id: "10", name: "Alfredo Pasta", image: food_10, price: 16, description: "Creamy white sauce pasta.", category: "Pasta" },
+  { _id: "11", name: "Penne Arrabiata", image: food_11, price: 15, description: "Spicy red sauce pasta.", category: "Pasta" },
+  { _id: "12", name: "Hakka Noodles", image: food_12, price: 12, description: "Stir-fried Indo-Chinese noodles.", category: "Noodles" },
+  { _id: "13", name: "Chow Mein", image: food_13, price: 13, description: "Chinese noodles with veggies.", category: "Noodles" },
+  { _id: "14", name: "Garlic Noodles", image: food_14, price: 12, description: "Noodles tossed with garlic sauce.", category: "Noodles" },
+  { _id: "15", name: "French Fries", image: food_15, price: 8, description: "Crispy golden fries.", category: "Fries" },
+  { _id: "16", name: "Masala Fries", image: food_16, price: 9, description: "Spiced Indian-style fries.", category: "Fries" },
+  { _id: "17", name: "Sweet Potato Fries", image: food_17, price: 10, description: "Healthier fry option.", category: "Fries" },
+  { _id: "18", name: "Fried Rice", image: food_18, price: 12, description: "Chinese-style rice with veggies.", category: "Rice" },
+  { _id: "19", name: "Jeera Rice", image: food_19, price: 10, description: "Simple cumin-flavored rice.", category: "Rice" },
+  { _id: "20", name: "Veg Biryani", image: food_20, price: 14, description: "Spiced layered rice dish.", category: "Rice" },
+  { _id: "21", name: "Gulab Jamun", image: food_21, price: 7, description: "Soft syrupy sweet.", category: "Dessert" },
+  { _id: "22", name: "Rasgulla", image: food_22, price: 7, description: "Juicy cottage cheese balls.", category: "Dessert" },
+  { _id: "23", name: "Chocolate Cake", image: food_23, price: 10, description: "Rich and moist chocolate cake.", category: "Dessert" },
+  { _id: "24", name: "Ice Cream Sundae", image: food_24, price: 9, description: "Classic vanilla with toppings.", category: "Dessert" },
+  { _id: "25", name: "Tiramisu", image: food_25, price: 11, description: "Coffee-flavored Italian dessert.", category: "Dessert" },
+  { _id: "26", name: "Fruit Salad", image: food_26, price: 8, description: "Healthy mix of seasonal fruits.", category: "Salad" },
+  { _id: "27", name: "Club Sandwich", image: food_27, price: 13, description: "Triple-layer chicken sandwich.", category: "Burger" },
+  { _id: "28", name: "Mac & Cheese", image: food_28, price: 14, description: "Cheesy pasta comfort food.", category: "Pasta" },
+  { _id: "29", name: "Spring Rolls", image: food_29, price: 10, description: "Crispy veggie rolls.", category: "Fries" },
+  //{ _id: "30", name: "Paneer Tikka", image: food_30, price: 15, description: "Grilled Indian cottage cheese.", category: "Salad" },
+];
+
+
+
+export const url = 'http://localhost:4000';
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'> 
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur ullam fugiat ab magni, temporibus aliquid. Ipsum laboriosam sequi ullam ad autem corrupti dicta! Perferendis, reprehenderit quaerat dolorem a laborum ut.</p>
+            <button>View Menu</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
